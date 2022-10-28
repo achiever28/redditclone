bf434cdf3dbdfbca068252c31e092b7552387f1e
achiever28 <thor280598@gmail.com>
2022-10-28 11:48:42 +0530

    first commit

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -13,8 +13,8 @@ const SearchInput:React.FC<SearchInputProps> = () => {
             <InputGroup>
                 <InputLeftElement
                 pointerEvents='none'
+                children={<PhoneIcon color='gray.400' mb={1} />}
                 />
-                <PhoneIcon color='gray.400' mb={1} />
                 <Input placeholder='Search Reddit' fontSize="10pt" _placeholder={{ color: "gray.500"}} _hover={{ bg:'white', border: '1px solid', borderColor: 'blue.500' }} _focus={{ outline:'none', border: '1px solid', borderColor: 'blue.500' }} height='34px' bg='gray.50'/>
             </InputGroup>
         </Flex>
