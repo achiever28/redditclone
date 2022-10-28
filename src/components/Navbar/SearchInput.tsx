0a5eaa83ab65b0d73b1a50e4e30e077464d4ae82
achiever28 <thor280598@gmail.com>
2022-10-28 12:51:30 +0530

    Bug FIx

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { PhoneIcon } from '@chakra-ui/icons';
+import { PhoneIcon, SearchIcon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import React from 'react';
 
@@ -12,10 +12,13 @@ const SearchInput:React.FC<SearchInputProps> = () => {
     return (
         <Flex flexGrow={1} mr={2} alignItems="center" > 
             <InputGroup>
-                <InputLeftElement
-                pointerEvents='none'
-                children={[<PhoneIcon color='gray.400' mb={1} />]}
-                />
+            <InputLeftElement
+          pointerEvents="none"
+          color="gray.400"
+          // children={<SearchIcon mb={2} />}
+        >
+          <SearchIcon mb={2} />
+        </InputLeftElement>
                 <Input placeholder='Search Reddit' fontSize="10pt" _placeholder={{ color: "gray.500"}} _hover={{ bg:'white', border: '1px solid', borderColor: 'blue.500' }} _focus={{ outline:'none', border: '1px solid', borderColor: 'blue.500' }} height='34px' bg='gray.50'/>
             </InputGroup>
         </Flex>
